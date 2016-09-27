e201438b4eed437f8729c928982453312ab9045a
urnmo <aaron0moore@gmail.com>
2016-09-27 09:20:48 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+$stateProvider.state({
+    name: 'music',
+    url: '/music',
+    template: '<h2>list view</h2>',
+    component: ''
+});
+
+$stateProvider.state({
+    name: 'friends',
+    url: '/friends',
+    template: '<h2>list view</h2>',
+    component: ''
+});
+
+$stateProvider.state({
+    name: 'home',
+    url: '/home',
+    template: '<h2>list view</h2>',
+    component: ''
+});
+
+app.component('music', {
+    
+})
